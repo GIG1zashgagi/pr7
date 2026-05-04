44cf63ec1d3f78ec26a75c674326155b15d9bc37
109234850921385 <iazavadskij15@gmail.com>
2026-05-04 09:09:10 +0300

    Условная точка останова здесь - проверьте, что происходит при вводе "abc"
    Преобразуем строку в число

--- pr7/pr7.cpp
+++ pr7/pr7.cpp
@@ -18,3 +18,25 @@ void PrintFibonacci(int n)
 }
 
 int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    cout << "Программа для вычисления чисел Фибоначчи" << endl;
+    cout << "Введите количество чисел для вывода: ";
+
+    string input;
+    cin >> input;
+
+    int count;
+
+    try
+    {
+        count = stoi(input);
+    }
+    catch (...)
+    {
+        cout << "Ошибка: введите целое число!" << endl;
+        system("pause");
+        return 1;
+    }
+}
