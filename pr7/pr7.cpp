a8b54631e97640ff8bcde9e89c89013417feeb67
109234850921385 <iazavadskij15@gmail.com>
2026-05-04 09:07:23 +0300

    Здесь вычисляется следующий член последовательности

--- pr7/pr7.cpp
+++ pr7/pr7.cpp
@@ -2,4 +2,17 @@
 #include <string>
 
 using namespace std;
+void PrintFibonacci(int n)
+{
+    int prev = 0;
+    int current = 1;
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << prev << " ";
+
+        int next = prev + current;
+        prev = current;
+        current = next;
+    }
+}
